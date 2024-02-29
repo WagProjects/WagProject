3ccc62174bd3fc14d6d478c86fad5c7b0c619e0f
Wagner Oliveira <contato-wagneros@outlook.com>
2024-02-29 19:36:38 -0300

    Footer

--- app/dist/js/src/sections/rodape/footer.js
+++ app/dist/js/src/sections/rodape/footer.js
@@ -1,9 +1,21 @@
 export function criarElementoRodaPe() {
     const sectionFooter = document.createElement('footer');
     sectionFooter.classList.add('rodape');
-    const cardFooter = document.createElement('div');
+    const cardFooter = document.createElement('section');
     cardFooter.classList.add('card__footer');
-    sectionFooter.innerHTML = ``;
+    cardFooter.innerHTML = `
+        <span>
+            <img src="./public/assets/images/Logo-02.png" alt="Logo Footer"/>
+            <h1>agner Oliveira</h1>
+        </span>
+        <div>
+            <ul>
+                <li><a>Sobre mim</a></li>
+                <li><a>Contato</a></li>
+            </ul>
+            <p>Wagner Oliveira | 2024 © Todos Direitos Reservados.</p>
+        </div>
+    `;
     sectionFooter.appendChild(cardFooter);
     return sectionFooter;
 }
