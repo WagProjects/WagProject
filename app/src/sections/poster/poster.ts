07d810230e458f8d17eeae2583f1a6c2e69d3e61
Wagner Oliveira <contato-wagneros@outlook.com>
2024-03-12 18:04:52 -0300

    Upgrade Poster

--- app/src/sections/poster/poster.ts
+++ app/src/sections/poster/poster.ts
@@ -6,6 +6,19 @@ export function criarElementoPoster(){
         <video class="video__intro " loop="loop" muted="muted" autoplay>
             <source src="../public/assets/images/backgrounds/background-banner.mp4" type="video/mp4">
         </video>
+        <div class="card__poster">
+            <div>
+                <h1>Zoio</h1>
+            </div>
+            <div class="poster__buttons">
+                <div class="rede__button git">
+                    <a href="https://github.com/WagProjects" target="_blank"><img class="icon__rede" alt="Icone do github" src="../public/assets/images/icons/Icon-square-github.svg"/>Github</a>
+                </div>
+                <div class="rede__button linkedin">
+                    <a href="https://www.linkedin.com/in/wagner-oliveira-b3959a170/" target="_blank"><img class="icon__rede" alt="Icone do linkedin" src="../public/assets/images/icons/Icon-linkedin.svg"/>Linkedin</a>
+                </div>
+            </div>
+        </div>
         <div class="poster__setas">
             <div class="seta__avancar1"></div>
             <div class="seta__avancar2"></div>
@@ -13,6 +26,19 @@ export function criarElementoPoster(){
         </div>
     `
 
+    const gitButton = sectionPoster.querySelector('.rede__button.git');
+    const linkedinButton = sectionPoster.querySelector('.rede__button.linkedin');
 
+    if (gitButton !== null) {
+        gitButton.addEventListener('click', () => redirecionaLink('https://github.com/WagProjects'));
+    }
+
+    if (linkedinButton !== null) {
+        linkedinButton.addEventListener('click', () => redirecionaLink('https://www.linkedin.com/in/wagner-oliveira-b3959a170/'));
+    }
+
+    function redirecionaLink(url: string) {
+        window.open(url, '_blank');
+    }
     return sectionPoster
 }
